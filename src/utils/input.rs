a6cb49e091bdefc78ab1e8a57764e70243b6d27a
filipelbc <filipelbc@gmail.com>
2020-11-07 23:09:16 -0300

    Trim and validate input

--- src/utils/input.rs
+++ src/utils/input.rs
@@ -4,6 +4,15 @@ use dialoguer::Input;
 pub fn prompt(p: &str) -> Result<String> {
     Input::new()
         .with_prompt(p)
+        .allow_empty(true)
+        .validate_with(|x: &String| {
+            if x.trim().is_empty() {
+                Err(format!("{} cannot be empty", p))
+            } else {
+                Ok(())
+            }
+        })
         .interact_text()
+        .map(|x| x.trim().to_string())
         .context("Could not read task title")
 }
